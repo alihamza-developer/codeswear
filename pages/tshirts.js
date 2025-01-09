4982fd7a658aaa889fd45b53461af0a1d937fe95
Ali Hamza <alihamzaofficial3536@gmail.com>
2025-01-10 00:51:20 +0500

    updates

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -2,9 +2,29 @@ import React from 'react'
 
 const Tshirts = () => {
   return (
-    <div>
 
-    </div>
+    <section className="text-gray-600 body-font">
+
+      <div className="container px-5 py-16 mx-auto">
+        <div className="flex flex-wrap -m-4">
+
+          {/* Single Product */}
+          <div className="lg:w-1/4 md:w-1/2 p-4 w-full rounded transition cursor-pointer hover:shadow-md">
+            <a className="block relative h-fit cursor-pointer rounded overflow-hidden">
+              <img alt="ecommerce" className="object-cover object-top w-full h-full block" src="/uploads/shirt1.jpg" />
+            </a>
+            
+            <div className="mt-4 text-center md:text-left">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+              <p className="mt-1">$16.00</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
   )
 }
 
