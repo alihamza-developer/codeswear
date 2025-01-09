6626d3fd641fd4ddc2f24fd7c44d2ba43d7cc2af
Ali Hamza <alihamzaofficial3536@gmail.com>
2025-01-09 23:39:16 +0500

    footer completed

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
+import { GoSidebarExpand } from "react-icons/go";
+
 
 const Navbar = () => {
   return (
-    <header className="text-gray-600 body-font border-b-2">
+    <nav className="text-gray-600 bg-white body-font border-b sticky top-0 mb-5">
 
       <div className="mx-5 flex justify-between p-3 items-center">
 
@@ -12,12 +15,11 @@ const Navbar = () => {
           <span className="ml-2 text-xl">CodesWear</span>
         </a>
 
-        <nav className="hidden md:flex flex-wrap cursor-pointer items-center text-base justify-center ">
-          <a href='/' className="mr-5 hover:text-gray-900">First Link</a>
-          <a href='/' className="mr-5 hover:text-gray-900">Second Link</a>
-          <a href='/' className="mr-5 hover:text-gray-900">Third Link</a>
-          <a href='/' className="mr-5 hover:text-gray-900">Fourth Link</a>
-        </nav>
+        <ul className="hidden md:flex flex-wrap cursor-pointer items-center text-base justify-center ">
+          <Link href="/"><li className="mr-5 hover:text-gray-900">Home</li></Link>
+          <Link href="/about"><li className="mr-5 hover:text-gray-900">About</li></Link>
+          <Link href="/contact"><li className="mr-5 hover:text-gray-900">Contact</li></Link>
+        </ul>
 
         <div className='hidden lg:block'>
           <button className="mx-1 inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
@@ -32,9 +34,10 @@ const Navbar = () => {
           </button>
         </div>
 
-        <button className='block md:hidden'>Open</button>
+        <button className='block md:hidden'><GoSidebarExpand size={20} /></button>
       </div>
-    </header>
+
+    </nav>
   )
 }
 
